3d271ffd5d6657edcac107bc208b2d6aad660586
BugHunter-X <askzenai@gmail.com>
2024-12-10 10:10:07 +0100

    Add expoBug.js file

---
+++ expoBug.js
@@ -0,0 +1,1 @@
+This error typically occurs when there's a mismatch between the versions of Expo CLI and the project's dependencies. It might also be caused by corrupted cache or incorrect configurations within the project.
